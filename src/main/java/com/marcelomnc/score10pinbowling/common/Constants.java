1b77df46689c54b4b9b4423f9ab40d694c5a86ec
marcelomnc <marcelo@pig.gi>
2020-06-26 11:24:29 -0500

    Constant names change. :bowtie:

--- src/main/java/com/marcelomnc/score10pinbowling/common/Constants.java
+++ src/main/java/com/marcelomnc/score10pinbowling/common/Constants.java
@@ -3,14 +3,14 @@ package com.marcelomnc.score10pinbowling.common;
 public class Constants {
     public static final String PLAYER_CHANCES_FILE__FIELD_SEPARATOR = "\t";
     public static final String PLAYER_CHANCES_FILE__FOUL_INDICATOR = "F";
-    public static final String PINFALLS__VALUE_SEPARATOR = " ";
-    public static final String PINFALLS__NO_CHANCE_INDICATOR = "*";
-    public static final String PINFALLS__FOUL_INDICATOR = "F";
-    public static final String PINFALLS__SPARE_INDICATOR = "/";
-    public static final String PINFALLS__STRIKE_INDICATOR = "X";
-    public static final String SCORE__FRAMES_LINE_LABEL = "Frame";
-    public static final String SCORE__FRAMES_LINE_SEPARATOR = "  ";
-    public static final String SCORE__PINFALLS_LINE_LABEL = "Pinfalls";
-    public static final String SCORE__SCORE_LINE_LABEL = "Score";
-    public static final String SCORE__SCORE_LINE_VALUE_SEPARATOR = "  ";
+    public static final String PRINT__FRAME_LABEL = "Frame";
+    public static final String PRINT__FRAME_SEPARATOR = "  ";//Two whitespaces
+    public static final String PRINT__PINFALLS_LABEL = "Pinfalls";
+    public static final String PRINT__PINFALLS_VALUE_SEPARATOR = " ";//One whitespace
+    public static final String PRINT__PINFALLS_CHANCE_SKIPPED_INDICATOR = "*";
+    public static final String PRINT__PINFALLS_FOUL_INDICATOR = "F";
+    public static final String PRINT__PINFALLS_SPARE_INDICATOR = "/";
+    public static final String PRINT__PINFALLS_STRIKE_INDICATOR = "X";
+    public static final String PRINT__SCORE_LABEL = "Score";
+    public static final String PRINT__SCORE_SEPARATOR = "  ";//Two whitespaces
 }
--- src/main/java/com/marcelomnc/score10pinbowling/printer/GamesPrinter.java
+++ src/main/java/com/marcelomnc/score10pinbowling/printer/GamesPrinter.java
@@ -10,15 +10,15 @@ public class GamesPrinter {
     public void printGames(Map<String, GameDTO> games) {
         //Printing frames header
         System.out.println("\n-----------------------------------------------------------------\n");
-        System.out.print(Constants.SCORE__FRAMES_LINE_LABEL);
+        System.out.print(Constants.PRINT__FRAME_LABEL);
         for (int i = 1; i <= 9; i++) {
-            System.out.print(Constants.SCORE__FRAMES_LINE_SEPARATOR + i);
+            System.out.print(Constants.PRINT__FRAME_SEPARATOR + i);
         }
-        System.out.println(Constants.SCORE__FRAMES_LINE_SEPARATOR + 10);
+        System.out.println(Constants.PRINT__FRAME_SEPARATOR + 10);
 
         games.forEach((gameOwner, gameDTO) -> {
             System.out.println(gameDTO.getOwnerName());
-            System.out.print(Constants.SCORE__PINFALLS_LINE_LABEL + Constants.PINFALLS__VALUE_SEPARATOR);
+            System.out.print(Constants.PRINT__PINFALLS_LABEL + Constants.PRINT__PINFALLS_VALUE_SEPARATOR);
 
             //Printing pinfalls
             for (int i = 0; i < gameDTO.getFrameDTOS().size(); i++) {
@@ -27,44 +27,44 @@ public class GamesPrinter {
                 if (i == 9) {
                     //10th frame (last)
                     if (frame.getPinFalls().get(0).isFoul()) {
-                        toPrint1 = Constants.PINFALLS__FOUL_INDICATOR + Constants.PINFALLS__VALUE_SEPARATOR;
+                        toPrint1 = Constants.PRINT__PINFALLS_FOUL_INDICATOR + Constants.PRINT__PINFALLS_VALUE_SEPARATOR;
                     } else if (frame.getPinFalls().get(0).getValue() == 10) {
-                        toPrint1 = Constants.PINFALLS__STRIKE_INDICATOR + Constants.PINFALLS__VALUE_SEPARATOR;
+                        toPrint1 = Constants.PRINT__PINFALLS_STRIKE_INDICATOR + Constants.PRINT__PINFALLS_VALUE_SEPARATOR;
                     } else {
-                        toPrint1 = frame.getPinFalls().get(0).getValue() + Constants.PINFALLS__VALUE_SEPARATOR;
+                        toPrint1 = frame.getPinFalls().get(0).getValue() + Constants.PRINT__PINFALLS_VALUE_SEPARATOR;
                     }
 
                     if (frame.getPinFalls().get(1).isFoul()) {
-                        toPrint2 = Constants.PINFALLS__FOUL_INDICATOR + Constants.PINFALLS__VALUE_SEPARATOR;
+                        toPrint2 = Constants.PRINT__PINFALLS_FOUL_INDICATOR + Constants.PRINT__PINFALLS_VALUE_SEPARATOR;
                     } else if (frame.getPinFalls().get(1).getValue() == 10) {
-                        toPrint2 = Constants.PINFALLS__STRIKE_INDICATOR + Constants.PINFALLS__VALUE_SEPARATOR;
+                        toPrint2 = Constants.PRINT__PINFALLS_STRIKE_INDICATOR + Constants.PRINT__PINFALLS_VALUE_SEPARATOR;
                     } else if ((frame.getPinFalls().get(0).getValue() + frame.getPinFalls().get(1).getValue()) == 10) {
-                        toPrint2 = Constants.PINFALLS__SPARE_INDICATOR + Constants.PINFALLS__VALUE_SEPARATOR;
+                        toPrint2 = Constants.PRINT__PINFALLS_SPARE_INDICATOR + Constants.PRINT__PINFALLS_VALUE_SEPARATOR;
                     } else {
-                        toPrint2 = frame.getPinFalls().get(1).getValue() + Constants.PINFALLS__VALUE_SEPARATOR;
+                        toPrint2 = frame.getPinFalls().get(1).getValue() + Constants.PRINT__PINFALLS_VALUE_SEPARATOR;
                     }
 
                     if (frame.getPinFalls().size() == 3) {
                         if (frame.getPinFalls().get(2).isFoul()) {
-                            toPrint3 = Constants.PINFALLS__FOUL_INDICATOR + Constants.PINFALLS__VALUE_SEPARATOR;
+                            toPrint3 = Constants.PRINT__PINFALLS_FOUL_INDICATOR + Constants.PRINT__PINFALLS_VALUE_SEPARATOR;
                         } else if (frame.getPinFalls().get(2).getValue() == 10) {
-                            toPrint3 = Constants.PINFALLS__STRIKE_INDICATOR + Constants.PINFALLS__VALUE_SEPARATOR;
+                            toPrint3 = Constants.PRINT__PINFALLS_STRIKE_INDICATOR + Constants.PRINT__PINFALLS_VALUE_SEPARATOR;
                         } else {
-                            toPrint3 = frame.getPinFalls().get(2).getValue() + Constants.PINFALLS__VALUE_SEPARATOR;
+                            toPrint3 = frame.getPinFalls().get(2).getValue() + Constants.PRINT__PINFALLS_VALUE_SEPARATOR;
                         }
                     }
                 } else {
                     //Frames 1 to 9
                     if (frame.getPinFalls().size() == 1) {
-                        toPrint1 = Constants.PINFALLS__NO_CHANCE_INDICATOR + Constants.PINFALLS__VALUE_SEPARATOR;
-                        toPrint2 = Constants.PINFALLS__STRIKE_INDICATOR;
+                        toPrint1 = Constants.PRINT__PINFALLS_CHANCE_SKIPPED_INDICATOR + Constants.PRINT__PINFALLS_VALUE_SEPARATOR;
+                        toPrint2 = Constants.PRINT__PINFALLS_STRIKE_INDICATOR;
                     } else {
-                        toPrint1 = frame.getPinFalls().get(0).isFoul() ? Constants.PINFALLS__FOUL_INDICATOR : String.valueOf(frame.getPinFalls().get(0).getValue());
-                        toPrint1 += Constants.PINFALLS__VALUE_SEPARATOR;
+                        toPrint1 = frame.getPinFalls().get(0).isFoul() ? Constants.PRINT__PINFALLS_FOUL_INDICATOR : String.valueOf(frame.getPinFalls().get(0).getValue());
+                        toPrint1 += Constants.PRINT__PINFALLS_VALUE_SEPARATOR;
 
-                        toPrint2 = frame.getPinFalls().get(1).isFoul() ? Constants.PINFALLS__FOUL_INDICATOR : String.valueOf(frame.getPinFalls().get(1).getValue());
+                        toPrint2 = frame.getPinFalls().get(1).isFoul() ? Constants.PRINT__PINFALLS_FOUL_INDICATOR : String.valueOf(frame.getPinFalls().get(1).getValue());
                         if ((frame.getPinFalls().get(0).getValue() + frame.getPinFalls().get(1).getValue()) == 10) {
-                            toPrint2 = Constants.PINFALLS__SPARE_INDICATOR;
+                            toPrint2 = Constants.PRINT__PINFALLS_SPARE_INDICATOR;
                         }
                     }
                 }
@@ -77,12 +77,12 @@ public class GamesPrinter {
                 } else {
                     //Frames 1 to 9
                     System.out.print(toPrint1);
-                    System.out.print(toPrint2 + Constants.PINFALLS__VALUE_SEPARATOR);
+                    System.out.print(toPrint2 + Constants.PRINT__PINFALLS_VALUE_SEPARATOR);
                 }
             }
 
             //Printing scores
-            System.out.print(Constants.SCORE__SCORE_LINE_LABEL + Constants.SCORE__SCORE_LINE_VALUE_SEPARATOR);
+            System.out.print(Constants.PRINT__SCORE_LABEL + Constants.PRINT__SCORE_SEPARATOR);
             for (int i = 0; i < gameDTO.getFrameDTOS().size(); i++) {
                 FrameDTO frameDTO = gameDTO.getFrameDTOS().get(i);
 
@@ -91,7 +91,7 @@ public class GamesPrinter {
                     System.out.println(frameDTO.getScore());
                 } else {
                     //Frames 1 to 9
-                    System.out.print(frameDTO.getScore() + Constants.SCORE__SCORE_LINE_VALUE_SEPARATOR);
+                    System.out.print(frameDTO.getScore() + Constants.PRINT__SCORE_SEPARATOR);
                 }
             }
         });
