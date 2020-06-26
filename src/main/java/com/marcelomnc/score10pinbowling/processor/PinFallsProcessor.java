a671ae84a7c493887589f63420c190bccebfaf80
marcelomnc <marcelo@pig.gi>
2020-06-26 11:11:02 -0500

    PinFalls processor logic improvement. :bowtie:

--- src/main/java/com/marcelomnc/score10pinbowling/processor/PinFallsProcessor.java
+++ src/main/java/com/marcelomnc/score10pinbowling/processor/PinFallsProcessor.java
@@ -9,64 +9,68 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class PinFallsProcessor {
-
     private static final Logger LOGGER = Logger.getLogger(PinFallsProcessor.class.getName());
 
     public void processPinFalls(Map<String, GameDTO> games) {
         games.forEach((gameOwner, gameDTO) -> {
             gameDTO.getPlayerChanceDTOs().forEach(playerChanceDTO -> {
                 if (gameDTO.getFrameDTOS().size() == 0) {
+                    //Add first frame to the game
                     this.addNewFrameToGame(gameDTO, null);
                 }
 
-                int actualFrameIndex = gameDTO.getFrameDTOS().size() - 1;
-                FrameDTO actualFrameDTO = gameDTO.getFrameDTOS().get(actualFrameIndex);
-                int actualPinFallIndex = actualFrameDTO.getPinFalls().size();
+                int currentFrameIndex = gameDTO.getFrameDTOS().size() - 1;
+                FrameDTO currentFrameDTO = gameDTO.getFrameDTOS().get(currentFrameIndex);
+                int currentPinFallIndex = currentFrameDTO.getPinFalls().size();
 
-                if (playerChanceDTO.getKnockedDownPins() == 10) {
-                    if (actualFrameIndex == 9) {
-                        //Its the last frame
-                        if (actualFrameDTO.getPinFalls().size() <= 2) {
-                            actualFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
-                        } else {
-                            //This is an error, because if this is the last frame, 3 chances are the maximum
-                            LOGGER.log(Level.SEVERE, "Player: " + gameDTO.getOwnerName() + ", exceeded chances.");
-                            return;
-                        }
+                if (currentFrameIndex == 9) {
+                    //10th frame (last)
+                    if (currentFrameDTO.getPinFalls().size() <= 1) {
+                        //First and second chance
+                        currentFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
+                    } else if (currentFrameDTO.getPinFalls().size() == 2
+                            &&
+                            (((currentFrameDTO.getPinFalls().get(0).getValue() == 10) || (currentFrameDTO.getPinFalls().get(1).getValue()) == 10)
+                                    || ((currentFrameDTO.getPinFalls().get(0).getValue() + currentFrameDTO.getPinFalls().get(1).getValue()) == 10))) {
+                        //Third chance only allowed when there is a strike or spare in the first two chances
+                        currentFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
                     } else {
-                        if (actualPinFallIndex == 0
-                                || (actualFrameDTO.getPinFalls().size() == 1 && actualFrameDTO.getPinFalls().get(0).getValue() == 0)) {
-                            //First chance of the frame, or if second chance, first one must have knocked down pins equal to zero
-                            actualFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
+                        //This is an error, because if this is the last frame, 3 chances are the maximum only when there is a strike or spare in the first two chances
+                        LOGGER.log(Level.SEVERE, "Player: " + gameDTO.getOwnerName() + ", exceeded chances.");
+                        return;
+                    }
+                } else {
+                    //Frames 1 to 9
+                    if (playerChanceDTO.getKnockedDownPins() == 10) {
+                        //Strike or spare chance
+                        if (currentPinFallIndex == 0
+                                || (currentFrameDTO.getPinFalls().size() == 1 && currentFrameDTO.getPinFalls().get(0).getValue() == 0)) {
+                            //First chance. If second chance, first one must have knocked down pins equal to zero to be a spare
+                            currentFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
                             this.addNewFrameToGame(gameDTO, null);
-                        } else if (actualFrameDTO.getPinFalls().size() == 1 && actualFrameDTO.getPinFalls().get(0).getValue() == 10) {
+                        } else if (currentFrameDTO.getPinFalls().size() == 1 && currentFrameDTO.getPinFalls().get(0).getValue() == 10) {
                             //The chance data is meant for the next frame, because the first chance was a strike
                             this.addNewFrameToGame(gameDTO, new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
                         } else {
                             //This is an error, because if this is the second chance, sum must be NOT greater than 10
-                            LOGGER.log(Level.SEVERE, "Player: " + gameDTO.getOwnerName() + ", frame: " + (actualFrameIndex + 1) + " exceeded pinfall sum.");
+                            LOGGER.log(Level.SEVERE, "Player: " + gameDTO.getOwnerName() + ", frame: " + (currentFrameIndex + 1) + " exceeded pinfall sum.");
                             return;
                         }
-                    }
-                } else {
-                    if (actualFrameIndex == 9) {
-                        //TODO: Its the last frame
-
                     } else {
-                        if (actualPinFallIndex == 0) {
-                            //First chance of the frame
-                            actualFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
-                        } else if (actualFrameDTO.getPinFalls().size() == 1
-                                && (actualFrameDTO.getPinFalls().get(0).getValue() + playerChanceDTO.getKnockedDownPins()) <= 10) {
-                            //Second chance of the frame, sum for the first and second must be NOT greater than 10
-                            actualFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
+                        if (currentPinFallIndex == 0) {
+                            //First chance.
+                            currentFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
+                        } else if (currentFrameDTO.getPinFalls().size() == 1
+                                && (currentFrameDTO.getPinFalls().get(0).getValue() + playerChanceDTO.getKnockedDownPins()) <= 10) {
+                            //Second chance, sum for the first and second must be NOT greater than 10
+                            currentFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
                             this.addNewFrameToGame(gameDTO, null);
-                        } else if (actualFrameDTO.getPinFalls().size() == 2) {
-                            //The scoreLine is meant for the next frame
+                        } else if (currentFrameDTO.getPinFalls().size() == 2) {
+                            //The chance data is meant for the next frame
                             this.addNewFrameToGame(gameDTO, new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
                         } else {
-                            //This is an error, because if this is the second attempt, sum will be more than 10
-                            LOGGER.log(Level.SEVERE, "Player: " + gameDTO.getOwnerName() + ", frame: " + (actualFrameIndex + 1) + " exceeded pinfall sum.");
+                            //This is an error, because if this is the second chance, sum will be more than 10
+                            LOGGER.log(Level.SEVERE, "Player: " + gameDTO.getOwnerName() + ", frame: " + (currentFrameIndex + 1) + " exceeded pinfall sum.");
                             return;
                         }
                     }
