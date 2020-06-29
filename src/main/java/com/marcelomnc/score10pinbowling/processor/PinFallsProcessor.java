c2b24f87d90189de6795c2cad7cfcc22eae39fa5
marcelomnc <marcelo@pig.gi>
2020-06-29 18:20:22 -0500

    Logic fix. :bowtie:

--- src/main/java/com/marcelomnc/score10pinbowling/processor/PinFallsProcessor.java
+++ src/main/java/com/marcelomnc/score10pinbowling/processor/PinFallsProcessor.java
@@ -30,32 +30,41 @@ public class PinFallsProcessor implements IPinFallsProcessor {
 
                 if (currentFrameIndex == 9) {
                     //10th frame (last)
-                    if (currentFrameDTO.getPinFalls().size() <= 1) {
-                        //First and second chance
+                    if (currentPinFallIndex == 0) {
+                        //First chance
                         currentFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
-                    } else if (currentFrameDTO.getPinFalls().size() == 2
+                    } else if (currentPinFallIndex == 1
                             &&
-                            (((currentFrameDTO.getPinFalls().get(0).getValue() == 10) || (currentFrameDTO.getPinFalls().get(1).getValue()) == 10)
-                                    || ((currentFrameDTO.getPinFalls().get(0).getValue() + currentFrameDTO.getPinFalls().get(1).getValue()) == 10))) {
-                        //Third chance only allowed when there is a strike or spare in the first two chances
+                            ((currentFrameDTO.getPinFalls().get(0).getValue() == 10)
+                                    || (currentFrameDTO.getPinFalls().get(0).getValue() + playerChanceDTO.getKnockedDownPins()) <= 10)) {
+                        //Second chance
+                        currentFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
+                    } else if (currentPinFallIndex == 2
+                            &&
+                            ((currentFrameDTO.getPinFalls().get(0).getValue() == 10)
+                                    || (currentFrameDTO.getPinFalls().get(0).getValue() + currentFrameDTO.getPinFalls().get(1).getValue()) == 10)) {
+                        //Third (extra) chance only allowed when there is a strike in the first chance, or a spare in the first two chances
                         currentFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
                     } else {
-                        //This is an error, because if this is the last frame, 3 chances are the maximum only when there is a strike or spare in the first two chances
                         gameDTO.setValid(false);
-                        LOGGER.log(Level.SEVERE, "Game for player: " + gameDTO.getPlayerName() + ", invalidated. Max chances exceeded");
-                        break;
+                        if (currentPinFallIndex == 1) {
+                            LOGGER.log(Level.SEVERE, "Game for player: " + gameDTO.getPlayerName() + ", invalidated. Frame " + (currentFrameIndex + 1) + ": exceeds pin falls max sum of 10");
+                        } else {
+                            //This is an error, because third (extra) chance is only allowed when there is a strike in the first chance, or a spare in the first two chances
+                            LOGGER.log(Level.SEVERE, "Game for player: " + gameDTO.getPlayerName() + ", invalidated. Max chances exceeded");
+                        }
                     }
                 } else {
                     //Frames 1 to 9
                     if (playerChanceDTO.getKnockedDownPins() == 10) {
                         //Strike or spare chance
                         if (currentPinFallIndex == 0
-                                || (currentFrameDTO.getPinFalls().size() == 1 && currentFrameDTO.getPinFalls().get(0).getValue() == 0)) {
+                                || (currentPinFallIndex == 1 && currentFrameDTO.getPinFalls().get(0).getValue() == 0)) {
                             //First chance. If second chance, first one must have knocked down pins equal to zero to be a spare
                             currentFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
                             this.addNewFrameToGame(gameDTO, null);
-                        } else if (currentFrameDTO.getPinFalls().size() == 1 && currentFrameDTO.getPinFalls().get(0).getValue() == 10) {
-                            //The chance data is meant for the next frame, because the first chance was a strike
+                        } else if (currentPinFallIndex == 1 && currentFrameDTO.getPinFalls().get(0).getValue() == 10) {
+                            //Second chance, chance data is meant for the next frame, because the first chance was a strike
                             this.addNewFrameToGame(gameDTO, new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
                         } else {
                             //This is an error, because if this is the second chance, sum must be NOT greater than 10
@@ -67,12 +76,12 @@ public class PinFallsProcessor implements IPinFallsProcessor {
                         if (currentPinFallIndex == 0) {
                             //First chance.
                             currentFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
-                        } else if (currentFrameDTO.getPinFalls().size() == 1
+                        } else if (currentPinFallIndex == 1
                                 && (currentFrameDTO.getPinFalls().get(0).getValue() + playerChanceDTO.getKnockedDownPins()) <= 10) {
                             //Second chance, sum for the first and second must be NOT greater than 10
                             currentFrameDTO.getPinFalls().add(new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
                             this.addNewFrameToGame(gameDTO, null);
-                        } else if (currentFrameDTO.getPinFalls().size() == 2) {
+                        } else if (currentPinFallIndex == 2) {
                             //The chance data is meant for the next frame
                             this.addNewFrameToGame(gameDTO, new PinFallDTO(playerChanceDTO.isFoul(), playerChanceDTO.getKnockedDownPins()));
                         } else {
@@ -86,9 +95,25 @@ public class PinFallsProcessor implements IPinFallsProcessor {
             }
 
             if (gameDTO.isValid()) {
-                if (gameDTO.getFrameDTOS().size() < 10
-                        || (gameDTO.getFrameDTOS().size() == 10
-                        && (gameDTO.getFrameDTOS().get(9).getPinFalls().size() < 2))) {
+                //Validate if there was not enough chances data to build all 10 frames completely
+                boolean invalidateGame = false;
+                if (gameDTO.getFrameDTOS().size() < 10) {
+                    invalidateGame = true;
+                } else if (gameDTO.getFrameDTOS().size() == 10) {
+                    FrameDTO lastFrame = gameDTO.getFrameDTOS().get(9);
+                    if (lastFrame.getPinFalls().size() < 2) {
+                        invalidateGame = true;
+                    } else if (lastFrame.getPinFalls().size() == 2
+                            &&
+                            ((lastFrame.getPinFalls().get(0).getValue() == 10)
+                                    || ((lastFrame.getPinFalls().get(0).getValue() == 10) && (lastFrame.getPinFalls().get(1).getValue() == 10))
+                                    || ((lastFrame.getPinFalls().get(0).getValue() + lastFrame.getPinFalls().get(1).getValue()) == 10))) {
+                        //Extra chance on last frame is missing
+                        invalidateGame = true;
+                    }
+                }
+
+                if (invalidateGame) {
                     //This is an error, player has not enough chance data on the file
                     gameDTO.setValid(false);
                     LOGGER.log(Level.SEVERE, "Game for player: " + gameDTO.getPlayerName() + ", invalidated. Not enough chances data");
